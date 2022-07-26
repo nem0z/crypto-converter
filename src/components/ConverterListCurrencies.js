fc08b952fdd9a798a5797ce41bf3423837e58128
nem0z <maxime.bernard.dev@gmail.com>
2022-07-27 00:09:40 +0200

    sync listCurrencies with Converter

--- src/components/ConverterListCurrencies.js
+++ src/components/ConverterListCurrencies.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 class ConverterListCurrencies extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			currencies: this.props.currencies,
-		};
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            currencies: null,
+        }
+    }
 
     updateListCurrencies(inputValue) {
-        const filteredCurrencies = this.state.currencies?.filter(c => c.includes(inputValue));
+        const filteredCurrencies = this.props.currencies.filter(c => c.includes(inputValue));
         this.setState({currencies: filteredCurrencies});
     }
 
@@ -17,10 +17,10 @@ class ConverterListCurrencies extends React.Component {
 
     	return (
 			<div>
-                { console.log(this.state) }
+                { console.log(this.props) }
                 <input type="text" onChange={ e => this.updateListCurrencies(e.target.value) } />
                 <ul>
-                    { this.state.currencies?.map(c => 
+                    { (this.state.currencies ?? this.props.currencies).map(c => 
                         <li key={c}>{ c }</li>
                     ) }
                 </ul>
