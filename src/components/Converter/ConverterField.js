f5649c098238ac49145d7729a55eda697cdd68bf
nem0z <maxime.bernard.dev@gmail.com>
2022-07-28 14:00:40 +0200

    ConverterField style

--- src/components/Converter/ConverterField.js
+++ src/components/Converter/ConverterField.js
@@ -29,7 +29,11 @@ class ConverterField extends React.Component {
 			<div>
                 <div className='ConverterField'>
                     <input type="text" value={this.props.value} onChange={e => this.props.onChange(this.props.currency, e.target.value)}/>
-                    <button onClick={ () => this.showPopUp() }>{ this.props.currency.symbol }</button>
+                    <button onClick={ () => this.showPopUp() }>
+                        <img src={this.props.currency.image} alt={this.props.currency.id}></img>
+                        <span>{ this.props.currency.symbol }</span>
+                        <span><svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg" class="sc-33m4yg-8 kgnnlF"><path d="M0.97168 1L6.20532 6L11.439 1" stroke="#AEAEAE"></path></svg></span>
+                    </button>
                 </div>
             
                 { 
