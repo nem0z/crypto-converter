17ec39ddee8acd2099ad7ed9644b3002779684a9
nem0z <maxime.bernard.dev@gmail.com>
2022-12-04 19:04:53 +0100

    Format (ConverterField): split svg to reduce line length

--- src/components/Converter/ConverterField.tsx
+++ src/components/Converter/ConverterField.tsx
@@ -36,7 +36,14 @@ export default function({currencies, current, onChange}: propsConvertField) {
                 <button onClick={ () => setShowList(true) }>
                     <img src={current.img} alt={ current.symb }></img>
                     <span>{ current.symb }</span>
-                    <span><svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.97168 1L6.20532 6L11.439 1" stroke="#AEAEAE"></path></svg></span>
+                    <span>
+                        <svg 
+                            width="12" height="7" viewBox="0 0 12 7" fill="none" 
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M0.97168 1L6.20532 6L11.439 1" stroke="#AEAEAE"></path>
+                        </svg>
+                    </span>
                 </button>
             </div>
         
